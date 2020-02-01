0bd7f2fc11e97d277b8f67c14d20583c3fd435fe
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-01 14:30:11 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy_all
+
+20.times do
+  Post.create(title:Faker::Book.title, description: Faker::TvShows::FamilyGuy.quote)
+end
